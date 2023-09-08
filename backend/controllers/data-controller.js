dc1fc3d8bb3fc84b14e2a8a00d2b905c1704e618
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-09-08 17:55:16 +0530

    lean function added

--- backend/controllers/data-controller.js
+++ backend/controllers/data-controller.js
@@ -23,7 +23,7 @@ const postnewdata = async (req, res) => {
             country: user.country,
         }));
 
-        const result = await Data.insertMany(userDataArray);
+        const result = await Data.insertMany(userDataArray).lean();
         res.status(201).json(result);
     } catch (error) {
         console.error("Error creating users:", error);
@@ -33,7 +33,7 @@ const postnewdata = async (req, res) => {
 
 const getalldata = async (req, res) => {
     try {
-        const data = await Data.findOne();
+        const data = await Data.findOne().lean;
         const selectedFields = ["username", "firstname", "lastname", "age", "email", "country"];
 
         const filteredData = selectedFields.reduce((result, field) => {
@@ -51,7 +51,7 @@ const getalldata = async (req, res) => {
 const getcsvdata = async (req, res) => {
     try {
         const fieldName = req.body;
-        const data = await Data.find({}, {...fieldName, _id: 0});
+        const data = await Data.find({}, {...fieldName, _id: 0}).lean();
         res.status(200).json(data);
     } catch (error) {
         console.error("There is some error named: ", error);
